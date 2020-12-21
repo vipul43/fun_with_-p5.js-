1f0d14dfb5bc4a277a3498645e9f2de283936be6
Sai Vipul Mohan <saifunny43@gmail.com>
2020-12-21 15:54:54 +0530

    - magic eight ball functions perfectly and returns random gif using   tenor api - more plans for magic eight ball: to return random yt link from   channels like business insider, clever programmer, the coding train,   physics girl etc - more plans for magic eight ball: to return random quotes - more plans for magic eight ball: to return unsplash images - also, to add quest and mine commands

--- discord_bot/index.js
+++ discord_bot/index.js
@@ -1,24 +1,62 @@
 require('dotenv').config();
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.CLIENT_LOGIN);
-
 client.on('ready', readyDiscord);
-function readyDiscord() {
-    console.log('Bot is online');
-}
-
 client.on('message', gotMessage);
-function gotMessage(msg) {
+
+const fetch = require('node-fetch');
+
+// log space
+
+
+async function gotMessage(msg) {
     // console.log(msg);
-    if(msg.content === '!hi'){
-        msg.reply('hi there');
-    }
-    if(msg.channel.id === process.env.DCP_CHANNEL_ID && msg.content === '!quest'){
-        msg.reply('didn\'t solve any question');
+    if(msg.content[0] === '!'){
+        if(msg.content === '!hi'){
+            reply(msg);
+        } else if(msg.channel.id === process.env.DCP_CHANNEL_ID && msg.content === '!quest'){
+            quest(msg);
+        } else if(msg.channel.id === process.env.DCP_CHANNEL_ID && msg.content === '!mine') {
+            mine(msg);
+        } else if(msg.content === '!8') {
+            eight(msg);
+        }
+
+
+        else {
+            trythese(msg);
+        }
     }
 }
-
-//ideas
-// 1) Magic 8 ball --> !8
-// 2) daily cp problem generator -> !mine && status of dcp -> !quest
+function readyDiscord() {
+    client.user.setActivity("you", { type: "LISTENING"})
+    console.log("Bot is online👍🏻");
+}
+function reply(msg){
+    msg.reply("hi there👋");
+}
+function quest(msg){
+    msg.reply("Here is your generated question");
+}
+function mine(msg){
+    msg.reply("You are on level x");
+}
+async function eight(msg){
+    const mood = generateRandomMood();
+    const url = `https://api.tenor.com/v1/random?q=${mood}&key=${process.env.TENOR_KEY}&limit=11&locale=en&ar_range=standard`;
+    const response = await fetch(url);
+    const json = await response.json();
+    const results = json.results;
+    // console.log(json);
+    msg.reply('shaking...'); //add loading animation here
+    msg.channel.send(results[Math.floor(Math.random() * results.length)].url);
+}
+function trythese(msg){
+    msg.reply('I didn\'t get what you are trying to say😅 \nTry these instead\n 👉🏻!hi\n👉🏻!mine\n👉🏻!quest');
+}
+function generateRandomMood(){
+    const categories = ["excited", "movies", "happy"];
+    return categories[Math.floor(Math.random() * categories.length)];
+}

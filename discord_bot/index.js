5a4750e76c2b5e838ddced70d7989f48a440f9a5
Sai Vipul Mohan <saifunny43@gmail.com>
2021-01-03 18:58:27 +0530

    added more features to eight, image gen and quote gen

--- discord_bot/index.js
+++ discord_bot/index.js
@@ -8,9 +8,10 @@ client.on('message', gotMessage);
 
 const fetch = require('node-fetch');
 
-// log space
-
-
+function readyDiscord() {
+    client.user.setActivity("you", { type: "LISTENING"})
+    console.log("Bot is online👍🏻");
+}
 async function gotMessage(msg) {
     // console.log(msg);
     if(msg.content[0] === '!'){
@@ -30,10 +31,6 @@ async function gotMessage(msg) {
         }
     }
 }
-function readyDiscord() {
-    client.user.setActivity("you", { type: "LISTENING"})
-    console.log("Bot is online👍🏻");
-}
 function reply(msg){
     msg.reply("hi there👋");
 }
@@ -44,19 +41,42 @@ function mine(msg){
     msg.reply("You are on level x");
 }
 async function eight(msg){
-    const mood = generateRandomMood();
+    const randChoice = Math.floor(Math.random() * 3);
+    if(randChoice==0){
+        gifGenerator(msg);
+    } else if(randChoice==1){
+        quoteGenerator(msg);
+    } else {
+        imageGenerator(msg);
+    }
+}
+function trythese(msg){
+    msg.reply('I didn\'t get what you are trying to say😅 \nTry these instead\n 👉🏻!hi\n👉🏻!mine\n👉🏻!quest');
+}
+
+// HELPER FUNCTIONS
+
+async function gifGenerator(msg){
+    const categories = ["excited", "movies", "happy", "smile", "laugh"];
+    const mood = categories[Math.floor(Math.random() * categories.length)];
     const url = `https://api.tenor.com/v1/random?q=${mood}&key=${process.env.TENOR_KEY}&limit=11&locale=en&ar_range=standard`;
     const response = await fetch(url);
     const json = await response.json();
     const results = json.results;
     // console.log(json);
-    msg.reply('shaking...'); //add loading animation here
-    msg.channel.send(results[Math.floor(Math.random() * results.length)].url);
+    msg.reply(`shaking...here is your gif to relax😍\n${results[Math.floor(Math.random() * results.length)].url}`);
 }
-function trythese(msg){
-    msg.reply('I didn\'t get what you are trying to say😅 \nTry these instead\n 👉🏻!hi\n👉🏻!mine\n👉🏻!quest');
+async function quoteGenerator(msg){
+    const url = `https://api.quotable.io/random`;
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(`${data.content} —${data.author}`);
+    msg.reply(`shaking...here is your quote to ponder💯\n"${data.content}" —${data.author}`);
 }
-function generateRandomMood(){
-    const categories = ["excited", "movies", "happy"];
-    return categories[Math.floor(Math.random() * categories.length)];
+async function imageGenerator(msg){
+    const url = `https://api.unsplash.com/photos/random/?client_id=${process.env.UNSPLASH_ACCESS_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(`${data.urls.regular}`);
+    msg.reply(`shaking...here is your image to enjoy👍🏻\n${data.urls.regular}`);
 }
